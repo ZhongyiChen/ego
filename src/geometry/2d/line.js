c8f9bfe4491f15751bfae0aed23933d7174f3f6f
chenzhongyi <ego@chenzhongyi.net>
2023-02-02 15:39:00 +0800

    实现了各个方式的对于 Line 的初始化

--- src/geometry/2d/line.js
+++ src/geometry/2d/line.js
@@ -52,8 +52,6 @@ export class Line {
    */
   constructor(x1, y1, x2, y2, config) {
     this.type = NaN;
-    this.x0 = NaN;
-    this.y0 = NaN;
     this.x1 = NaN;
     this.y1 = NaN;
     this.x2 = NaN;
@@ -81,19 +79,109 @@ export class Line {
       this._C = config.C;
       return this;
     }
+    if (LineType.SLOPE_INTERCEPT === config.type) {
+      this.type = LineType.SLOPE_INTERCEPT;
+      this._k = config.k;
+      this._b = config.b;
+      return this;
+    }
+    if (LineType.POINT_SLOPE === config.type) {
+      this.type = LineType.POINT_SLOPE;
+      this.x1 = config.x1;
+      this.y1 = config.y1;
+      this._k = config.k;
+      return this;
+    }
+    if (LineType.INTERCEPT_INTERCEPT === config.type) {
+      this.type = LineType.INTERCEPT_INTERCEPT;
+      this._a = config.a;
+      this._b = config.b;
+      return this;
+    }
   }
   /**
    * 两点式
+   * 
+   * @param {number} x1 - 起始点横坐标
+   * @param {number} y1 - 起始点纵坐标
+   * @param {number} x2 - 结束点横坐标
+   * @param {number} y2 - 结束点纵坐标
+   * 
+   * @returns {Line}
    */
   static fromPointPoint(x1, y1, x2, y2) {
     return new Line(x1, y1, x2, y2);
   }
   /**
-   * 斜截式
+   * 点斜式
+   * 
+   * @param {number} x1 - 某点横坐标
+   * @param {number} y1 - 某点纵坐标
+   * @param {number} k - 斜率
+   * 
+   * @returns {Line}
    */
+  static fromPointSlope(x1, y1, k) {
+    if (!k) throw new Error('k should not be 0');
+
+    return new Line(0, 0, 0, 0, {
+      type: LineType.POINT_SLOPE,
+      x1,
+      y1,
+      k,
+    });
+  }
+  /**
+   * 斜截式
+   * 
+   * @param {number} k - 斜率
+   * @param {number} b - y 轴截距
+   * 
+   * @returns {Line}
+   */
+  static fromSlopeIntercept(k, b) {
+    return new Line(0, 0, 0, 0, {
+      type: LineType.SLOPE_INTERCEPT,
+      k,
+      b,
+    });
+  }
+  /**
+   * 截距式
+   * 
+   * @param {number} a - x 轴截距
+   * @param {number} b - y 轴截距
+   * 
+   * @returns {Line}
+   */
+  static fromInterceptIntercept(a, b) {
+    if (!a || !b) throw new Error('a annd b should not be 0');
+
+    return new Line(0, 0, 0, 0, {
+      type: LineType.INTERCEPT_INTERCEPT,
+      a,
+      b,
+    });
+  }
   /**
    * 一般式
+   * 
+   * @param {number} A - x 的系数
+   * @param {number} B - y 的系数
+   * @param {number} C - 常数
+   * 
+   * @returns {Line}
    */
+  static fromGeneral(A, B, C) {
+    if (A === 0 && B === 0) throw new Error('A or B cannot be 0');
+
+    return new Line(0, 0, 0, 0, {
+      type: LineType.GENERAL,
+      A,
+      B,
+      C,
+    });
+  }
   /**
    * 长度值(作为线段看待时)
    * 
